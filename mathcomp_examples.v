132c51513acbcbe33078d4b518cd1ae7dc7b1c9e
VHarisop <the.machine.headed@gmail.com>
2017-04-03 21:41:20 +0300

    Added a couple more lemmas

--- mathcomp_examples.v
+++ mathcomp_examples.v
@@ -37,3 +37,47 @@ Proof.
     - rewrite !mulnS leq_add //=.
       apply IHn; apply n1_leq_n2.
 Qed.
+
+(* Trivial lemma about exponentiation. *)
+Lemma tut_expn_leq: forall a b n, a ^ n <= (a + b) ^ n.
+Proof.
+  (* Sublemma in case a is positive *)
+  have expn_leq: forall a b n, a > 0 -> a ^ n <= (a + b) ^ n.
+    move => a b n a_gt_0.
+    elim: n => [// | n IHn].
+    rewrite !expnS mulnDl.
+    rewrite (leq_trans (_ : _ <= (a * (a + b) ^ n))) //=.
+    - rewrite leq_pmul2l.
+        + apply: IHn.
+        + apply: a_gt_0.
+    - rewrite leq_addr //=.
+  move => a b n.
+  elim: a => [//= | a IHa].
+    - rewrite add0n.
+      elim: n => [// | n IHn].
+        + rewrite exp0n.
+          rewrite expnS leq0n //=.
+        + by [].
+        + by rewrite expn_leq.
+Qed.
+
+(* Toy Lemma about sum of terms. *)
+Lemma tut_sum_2_power_4: forall a b, a ^ 4 + b ^ 4 <= (a + b)^4.
+Proof.  
+  have expn_convex: forall a b n, a^(n.+1) + b^(n.+1) <= (a + b)^(n.+1).
+    - move => a b n.
+      (* expnS: a^{n+1} -> a * a ^ n
+         mulnDl: 'break' (a + b) * something
+         leq_add: use tut_expn_leq for both a and b *)
+      rewrite !expnS. rewrite mulnDl. rewrite leq_add //.
+        rewrite leq_mul2l.
+        elim: a => [// | a IHa].
+          by simpl; rewrite tut_expn_leq.
+        rewrite leq_mul2l.
+        elim: b => [// | b IHb].
+          by simpl; rewrite addnC tut_expn_leq.
+  (* Use expn_convex to prove trivial goal. *)
+  move => a b.
+  apply expn_convex.
+Qed.
+
