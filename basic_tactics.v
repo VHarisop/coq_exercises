4f13b4ede440166ed59491d47d67dc5faed50957
VHarisop <the.machine.headed@gmail.com>
2017-04-04 19:02:53 +0300

    Added session using basic tactics only

---
+++ basic_tactics.v
@@ -0,0 +1,74 @@
+From mathcomp Require Import all_ssreflect.
+
+(* Magic settings *)
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+Section simpleEx.
+(* Some variables *)
+Variables P Q R S : Prop.
+
+(* Identity *)
+Lemma id_P: P -> P.
+Proof.
+  move => p; apply p.
+Qed.
+
+(* Almost identity *)
+Lemma id_PP: (P -> P) -> P -> P.
+Proof.
+  move => pp; apply pp.
+Qed.
+
+(* Transitive identity *)
+Lemma imp_trans : (P -> Q) -> (Q -> R) -> P -> R.
+Proof.
+  move => pq qr p.
+  by apply qr; apply pq.
+Qed.
+
+Lemma imp_perm : (P -> Q -> R) -> Q -> P -> R.
+Proof.
+  move => pqr q p.
+  apply pqr.
+    - by apply p.
+    - by apply q.
+Qed.
+
+Lemma ignore_Q : (P -> R) -> P -> Q -> R.
+Proof.
+  move => pr p _.
+  apply pr; apply p.
+Qed.
+
+Lemma delta_imp : (P -> P -> Q) -> P -> Q.
+Proof.
+  move => ppq pq.
+  apply ppq; apply pq.
+Qed.
+
+Lemma delta_impR : (P -> Q) -> P -> P -> Q.
+Proof.
+  move => pq _; apply pq.
+Qed.
+
+Lemma diamond : (P -> Q) -> (P -> R) -> (Q -> R -> S) -> P -> S.
+Proof.
+  move => pq pr qrs p.
+  apply: qrs.
+    - apply pq; apply p.
+    - apply pr; apply p.
+Qed.
+
+Lemma weak_peirce : ((((P -> Q) -> P) -> P) -> Q) -> Q.
+Proof.
+  move => pqpp.
+  apply pqpp.
+  move => pqp.
+  apply pqp.
+  move => p.
+  apply pqpp.
+  (* I already have the next premise. *)
+  move => _; apply p.
+Qed.
