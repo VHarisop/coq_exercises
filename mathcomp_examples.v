48fe18ea9bcf171960e5c37784b69a008729c1f2
VHarisop <the.machine.headed@gmail.com>
2017-04-04 15:30:59 +0300

    Added couple of examples that utilize reflection views

--- mathcomp_examples.v
+++ mathcomp_examples.v
@@ -63,21 +63,55 @@ Qed.
 
 (* Toy Lemma about sum of terms. *)
 Lemma tut_sum_2_power_4: forall a b, a ^ 4 + b ^ 4 <= (a + b)^4.
-Proof.  
+Proof.
+  have lemma_1: forall a b n, a * a ^ n <= a * (a + b) ^ n.
+    move => a b n.
+    rewrite leq_mul2l.
+    elim: a => [// | a IHa].
+    by simpl; rewrite tut_expn_leq.
   have expn_convex: forall a b n, a^(n.+1) + b^(n.+1) <= (a + b)^(n.+1).
-    - move => a b n.
-      (* expnS: a^{n+1} -> a * a ^ n
-         mulnDl: 'break' (a + b) * something
-         leq_add: use tut_expn_leq for both a and b *)
-      rewrite !expnS. rewrite mulnDl. rewrite leq_add //.
-        rewrite leq_mul2l.
-        elim: a => [// | a IHa].
-          by simpl; rewrite tut_expn_leq.
-        rewrite leq_mul2l.
-        elim: b => [// | b IHb].
-          by simpl; rewrite addnC tut_expn_leq.
+    move => a b n.
+    (* expnS: a^{n+1} -> a * a ^ n
+       mulnDl: 'break' (a + b) * something
+       leq_add: use lemma_1 for both a and b *)
+      rewrite !expnS. rewrite mulnDl. rewrite leq_add //=.
+      rewrite addnC //=.
   (* Use expn_convex to prove trivial goal. *)
   move => a b.
   apply expn_convex.
 Qed.
 
+
+(* -------------------------------
+   Examples using reflection views
+   ------------------------------- *)
+
+(* Use this lemma to prove next, to show a usage scenario
+   of the 'case' tactic. *)
+Lemma tut_leq_total m n: (m <= n) || (n <= m).
+Proof.
+  rewrite -implyNb -ltnNge.
+  apply /implyP.
+  apply ltnW.
+Qed.
+
+(* Show that when m is less than max(n1, n2), it must be smaller than
+   at least one of n1, n2. *)
+Lemma tut_leq_max m n1 n2 : m <= (maxn n1 n2) = (m <= n1) || (m <= n2).
+Proof.
+  (* Below line is equivalent to:
+     - case: (leq_total n1 n2)
+       move => /orP [leq_n12 | leq_n21].
+     leq_total n1 n2 results in a || hypothesis that can be
+     changed to a \/ using the orP view, applied by /orP. The
+     \/ hypothesis is then able to be split using => [ha | hb]. *)
+  case/orP: (leq_total n1 n2) => [leq_n12 | leq_n21].
+      (* maxn_idPr: reflect (maxn m n = n) (m <= n).
+         Here, we provide the second premise as leq_n12 (n1 <= n2) *)
+    - rewrite (maxn_idPr leq_n12).
+      rewrite orb_idl // => le_mn1.
+      rewrite (leq_trans (_ : _ <= n1)) //.
+    - rewrite (maxn_idPl leq_n21).
+      rewrite orb_idr // => le_mn2.
+      rewrite (leq_trans (_ : _ <= n2)) //.
+Qed.
